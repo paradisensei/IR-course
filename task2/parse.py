b7c3cd7873631ffbc984c4dbd2a1c5ac1a46d18b
paradisensei <aidarshaifutdinov@gmail.com>
2018-02-20 20:21:31 +0300

    Task 2

---
+++ task2/parse.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+import io, json
+import requests
+from lxml import html
+
+# porter stemming algorithm
+from porter import Porter
+# Yandex Mystem 3.0 wrapper (pip install pymystem3)
+from pymystem3 import Mystem
+m = Mystem()
+
+# constants
+base_url = 'http://www.mathnet.ru'
+search_url = base_url + '/php/archive.phtml?jrnid=ivm&wshow=issue&year=2017&volume=&volume_alt=&issue=2&issue_alt=&option_lang=rus'
+
+result = {
+    'issue': search_url,
+    'articles': []
+}
+
+# get html page
+tree = html.fromstring(requests.get(search_url).content)
+
+# get <a> tags with links to articles
+articles = filter(
+    lambda a: a.get('href').startswith('/rus'),
+    tree.xpath("//a[contains(@class,'SLink')]")
+)
+
+# iterate over all articles & retrieve necessary data
+for a in articles:
+    url = base_url + a.get('href')
+    article = html.fromstring(requests.get(url).content)
+
+    title = a.text
+    title_porter = ' '.join(map(Porter.stem, title.split()));
+    title_mystem = ''.join(m.lemmatize(title))
+    
+    abstract = article.xpath(u"//b[contains(text(), 'Аннотация')]/following-sibling::text()")[0].strip()
+    abstract_porter = ' '.join(map(Porter.stem, abstract.split()));
+    abstract_mystem = ''.join(m.lemmatize(abstract))
+
+    keywords = article.xpath(u"//b[contains(text(), 'Ключевые')]/following-sibling::i")[0].text[:-1].split(', ')
+    result['articles'].append({
+        'url': url,
+        'title': {
+            'original': title,
+            'porter': title_porter,
+            'mystem': title_mystem
+        },
+        'abstract': {
+            'original': abstract,
+            'porter': abstract_porter,
+            'mystem': abstract_mystem
+        },
+        'keywords': keywords
+    })
+
+# save results to JSON file
+with io.open('result.json', 'w', encoding='utf-8') as out:
+    out.write(json.dumps(result,  indent=2, ensure_ascii=False))
